70c7a3b81df85fc879caffb20a97ffd491de44c0
CarlosDanielCastilloBanda <158379780+CarlosDanielCastilloBanda@users.noreply.github.com>
2024-02-08 18:51:48 -0600

    Add files via upload

---
+++ EVA1_3_MODIFICADORES_ACCESO/src/OTRO_PROYECTO/OTRO_PAQUETE.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OTRO_PROYECTO;
+
+/**
+ *
+ * @author acost
+ */
+public class OTRO_PAQUETE {
+    public int x;
+    protected int y;
+    private int z;
+    int w;
+    
+}
+
+class PruebaB{
+    public int x;
+    protected int y;
+    private int z;
+    int w;
+}
--- EVA1_3_MODIFICADORES_ACCESO/src/eva1_3_modificadores_acceso/EVA1_3_MODIFICADORES_ACCESO.java
+++ EVA1_3_MODIFICADORES_ACCESO/src/eva1_3_modificadores_acceso/EVA1_3_MODIFICADORES_ACCESO.java
@@ -1,22 +1,42 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package eva1_3_modificadores_acceso;
+
+import OTRO_PROYECTO.OTRO_PAQUETE;
+
+
 
 /**
  *
- * @author invitado
+ * @author acost
  */
 public class EVA1_3_MODIFICADORES_ACCESO {
+    public int x;
+    protected int y;
+    private int z;
+    int w;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+       PruebaA objA = new PruebaA();
+       //objA. --> puedo ver x (public)
+       //puedo ver a y (protected)
+       //puedo ver a w (default)
+       OTRO_PAQUETE otroObj = new OTRO_PAQUETE();
+       //los demas no son visibles 
+       //PruebaB objB = new PruebaB();
     
+}
+
+//CLASE CON ACCESO DEFAULT
+class PruebaA{
+    public int x;
+    protected int y;
+    private int z;
+    int w;
 }
